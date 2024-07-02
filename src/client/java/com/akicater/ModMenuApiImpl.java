a24ea8c31279b9f8354923be555d4e28a9f62b3e
daunita <markpustog@gmail.com>
2024-07-02 23:51:14 +0500

    minor changes

---
+++ src/client/java/com/akicater/ModMenuApiImpl.java
@@ -0,0 +1,16 @@
+package com.akicater;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ModMenuApiImpl implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(CUIConfig.class, parent).get();
+    }
+}
--- src/main/java/com/akicater/Cui.java
+++ src/main/java/com/akicater/Cui.java
@@ -1,14 +1,12 @@
 package com.akicater;
 
-import com.mojang.authlib.minecraft.client.MinecraftClient;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.text.TranslatableTextContent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Cui implements ModInitializer {
+	public static final String MODID = "cui";
     public static final Logger LOGGER = LoggerFactory.getLogger("cui");
 
 	@Override
